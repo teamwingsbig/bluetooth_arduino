047167d8c8f40418599ff38596c739ee7a3b9290
teamwingsbig <teamwingsbig@gmail.com>
2022-05-04 18:58:52 +0400

    cmtccsd

--- lib/saveDoctor.dart
+++ lib/saveDoctor.dart
@@ -111,6 +111,7 @@ class _SaveDoctor_ScreenState extends State<SaveDoctor_Screen> {
                         ),
                       ),
                       items: [
+
                         "Allergists/Immunologists",
                         "Anesthesiologists",
                         "Cardiologists",
